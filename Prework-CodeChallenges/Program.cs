fc875eb7230d6ab66ec2c7e37386fb02d3ee69a1
HamZa Samha <smha.hmza@gmail.com>
2022-03-12 11:58:31 +0300

    SolvedLeapYear

--- Prework-CodeChallenges/Program.cs
+++ Prework-CodeChallenges/Program.cs
@@ -4,9 +4,23 @@ namespace Prework_CodeChallenges
 {
     class Program
     {
+
+        bool IsLeapYear(int year)
+        {
+            if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+                return true;
+            else if (year % 4 == 0 && year % 100 == 0)
+                return false;
+            else if (year % 4 == 0)
+                return true;
+            else
+                return false;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Program().IsLeapYear(2000));
+            Console.WriteLine(new Program().IsLeapYear(1997));
         }
     }
 }
